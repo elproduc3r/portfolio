e268eff7082ceb3a146adc9382157626c1d2d64c
elproducer <elproducer20@gmail.com>
2023-05-23 14:57:59 -0700

    fix: remove mongoose

--- server/app.js
+++ server/app.js
@@ -4,30 +4,16 @@ const cors = require('cors');
 const colors = require('colors');
 const express = require('express');
 const path = require('path');
-// const {graphqlHTTP} = require('express-graphql');
-// const schema = require('./schema/schema');
-// const connectDb = require('./config/db');
+
 const port = process.env.PORT || "5000";
 const app = express();
 
-// connectDb();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
-
-// app.use(
-//   '/graphql',
-//   graphqlHTTP({
-//     schema,
-//     graphiql: process.env.NODE_ENV === 'development',
-//   })
-// );
 
 /* GET api */
 app.use('/api', function(req, res, next) {
--- server/public/scripts/main.js
+++ server/public/scripts/main.js
@@ -63,39 +63,6 @@
         });
     };
 
-
-   /* OffCanvas Menu
-    * ------------------------------------------------------ */
-    var clOffCanvas = function() {
-
-        var menuTrigger     = $('.header-menu-toggle'),
-            nav             = $('.header-nav'),
-            closeButton     = nav.find('.header-nav__close'),
-            siteBody        = $('body'),
-            mainContents    = $('section, footer');
-
-        // open-close menu by clicking on the menu icon
-        menuTrigger.on('click', function(e){
-            e.preventDefault();
-            siteBody.toggleClass('menu-is-open');
-        });
-
-        // close menu by clicking the close button
-        closeButton.on('click', function(e){
-            e.preventDefault();
-            menuTrigger.trigger('click');
-        });
-
-        // close menu clicking outside the menu itself
-        siteBody.on('click', function(e){
-            if( !$(e.target).is('.header-nav, .header-nav__content, .header-menu-toggle, .header-menu-toggle span') ) {
-                siteBody.removeClass('menu-is-open');
-            }
-        });
-
-    };
-
-
    /* photoswipe
     * ----------------------------------------------------- */
     var clPhotoswipe = function() {
@@ -354,7 +321,6 @@
         clPreloader();
         //menu
         clMenuOnScrolldown();
-        clOffCanvas();
 
         // clPhotoswipe();
         // clStatCount();
