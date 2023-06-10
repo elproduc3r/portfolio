9bce9148399cdd80f8555d624ecf9ef48a2059c9
elproducer <elproducer20@gmail.com>
2023-06-10 13:32:50 -0700

    fix: list build dir before copy

--- tasks/buildClient.sh
+++ tasks/buildClient.sh
@@ -4,6 +4,7 @@ echo "${GREEN}Running build script in /client${NC}"
 cd client
 npm run build
 echo ""
+ls -la build
 echo "${GREEN}Moving build files into node app: /server/public${NC}"
 # rm -rf ../server/public
 # mkdir ../server/public
